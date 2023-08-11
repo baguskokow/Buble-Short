19a17b315b89520bd328dd2992f1d0ae9de78ad7
koko <baguskokolearn@gmail.com>
2023-08-11 20:47:37 +0700

    Update : Rename buble short to buble sort

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-I tried the buble short algorithm in C++. I used array data from unordered values to sorted values. <br>
+I tried the buble sort algorithm in C++. I used array data from unordered values to sorted values. <br>
 This is sample output of my code. <br>
 ![sample-output](./image/sample-output.png)
--- bublesort.cpp
+++ bublesort.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void bubleShort(int myarray[], int n) {
+void bubleSort(int myarray[], int n) {
 	int temp;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n - 1; j++) {
@@ -26,7 +26,7 @@ int main() {
 	int myarray[6] = {5, 4, 3, 6, 2, 7};
 	int n = sizeof(myarray) / sizeof(myarray[0]);
 
-	bubleShort(myarray, n);
+	bubleSort(myarray, n);
 	printArray(myarray, n);
 
 	return 0;
